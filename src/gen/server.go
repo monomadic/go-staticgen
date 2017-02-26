0265064533bfa33cc84b6d4d5635a4834b6bc3cb
Rob Saunders <rob@wearebrandnew.com>
2017-02-26 19:52:49 +0700

    server now complains if you don't run build first.

--- src/gen/server.go
+++ src/gen/server.go
@@ -3,9 +3,16 @@ package main
 import (
   "log"
   "net/http"
+  "os"
 )
 
 func serve() {
+  if _, err := os.Stat("public"); os.IsNotExist(err) {
+    consoleError(err)
+    println("Directory ./public does not exist. Please run gen build first.")
+    os.Exit(1)
+  }
+
   fs := http.FileServer(http.Dir("public"))
   http.Handle("/", fs)
 
